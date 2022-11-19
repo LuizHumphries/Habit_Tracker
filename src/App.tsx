4ecb80e28fc65b24af37d57903e0f0e172e0262b
Jéssica Cottim <luizgustavo.frota@outlook.com>
2022-11-19 11:46:19 -0300

    Start

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Home } from "./pages/Home";
+import './styles/tailwind.css'
+
+
+export default function App() {
+  return (
+    <Home />
+  )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    linearBorderGradients: {
+      directions: {
+        // defaults to these values
+        t: "to top",
+        tr: "to top right",
+        r: "to right",
+        br: "to bottom right",
+        b: "to bottom",
+        bl: "to bottom left",
+        l: "to left",
+        tl: "to top left",
+      },
+      colors: {
+        // defaults to {}
+        "red-red": ["#fb7185", "#881337", "#fb7185"],
+      },
+      background: {
+        slate: "#F8fafc",
+      },
+      border: {
+        // defaults to these values (optional)
+        1: "1px",
+        2: "2px",
+        4: "4px",
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins"],
+    },
+    extend: {},
+  },
+  variants: {
+    linearBorderGradients: ["responsive", "hover", "dark"],
+  },
+  plugins: [require("tailwindcss-border-gradient-radius")],
+};
